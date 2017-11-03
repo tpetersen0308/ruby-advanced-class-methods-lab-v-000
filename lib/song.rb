c22d98d2738de96733925ebed226f2528ec8654d
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-03 00:14:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,4 +30,8 @@ class Song
     self.all.detect { |title| title.name == name }
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
 end
